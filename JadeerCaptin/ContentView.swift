eca5375556dfa266c5f324961ddf597a8c2c0545
awad sandal <awad619@hotmail.com>
2022-01-15 13:51:08 +0300

    Initial Commit

---
+++ JadeerCaptin/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JadeerCaptin
+//
+//  Created by mohammed almalki           on 12/06/1443 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JadeerCaptin/JadeerCaptinApp.swift
@@ -0,0 +1,17 @@
+//
+//  JadeerCaptinApp.swift
+//  JadeerCaptin
+//
+//  Created by mohammed almalki           on 12/06/1443 AH.
+//
+
+import SwiftUI
+
+@main
+struct JadeerCaptinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
